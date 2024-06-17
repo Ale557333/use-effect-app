cafde3ed5674e67205f6f33a3d528da4b336a19e
Alexandre Oliveira <alexandre.yukon@gmail.com>
2024-06-17 13:32:47 -0400

    buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
-function App() {
+const App  = () => {
+  const [resourceType, setResourceType] = useState('posts');
+
+  const changeResourceType = (resourceType) => {
+    setResourceType(resourceType);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{resourceType}</h1>
+      <div style={{ display: 'flex', alignItems: "center" }}>
+        <button onClick={() => changeResourceType('posts')}>Posts</button>
+        <button onClick={() => changeResourceType('comments')}>Comments</button>
+        <button onClick={() => changeResourceType('todos')}>Todos</button>
+       </div> 
     </div>
   );
-}
+};
 
 export default App;
