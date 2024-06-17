92c0429e087bc965728785064777d383cd2926cc
Alexandre Oliveira <alexandre.yukon@gmail.com>
2024-06-17 14:44:08 -0400

    fecth api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App  = () => {
+  const [items, setItems] = useState([]); // [state, setState
   const [resourceType, setResourceType] = useState('posts');
+
+  useEffect(() => {
+    const fetchResourceTypes = async () => {
+      const response = await fetch(
+        `https://jsonplaceholder.typicode.com/${resourceType}`
+      );
+      const json = await response.json();
+      setItems(json);
+    };
+
+    fetchResourceTypes();
+  }, [resourceType]);
 
   const changeResourceType = (resourceType) => {
     setResourceType(resourceType);
@@ -15,8 +28,13 @@ const App  = () => {
         <button onClick={() => changeResourceType('comments')}>Comments</button>
         <button onClick={() => changeResourceType('todos')}>Todos</button>
        </div> 
-    </div>
   );
+
+     {items.map((item) => (
+       <p>{item.title}</p>
+       ))}
+    </div>
+  );  
 };
 
 export default App;
